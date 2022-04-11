40af5c378ac533b045775a97c7ae474d540845ff
samarbekova-source <chikasamarbekova@gmail.com>
2022-04-11 15:26:22 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import "antd/dist/antd.css";
+import Routing from "./Routing";
+
+function App() {
+  return (
+    <div className="App">
+      <Routing />
+    </div>
+  );
+}
+
+export default App;
+
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+
+// useEffect(() => {
+//   AOS.init();
+//   AOS.refresh();
+// }, []);
